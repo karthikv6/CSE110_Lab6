45b7af5453026202104e8e4bb6e161add1fb2abc
karthikv6 <44593407+karthikv6@users.noreply.github.com>
2022-11-07 23:39:13 -0800

    commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -61,7 +61,7 @@ function getRecipesFromStorage() {
   //   }
   // ]
   var request = new XMLHttpRequest();
-  request.open("GET","../CSE110_Lab6/reference/recipes.json", false);
+  request.open("GET","./reference/recipes.json", false);
   request.send(null);
   var d = JSON.parse(request.responseText);
   console.log(d);
