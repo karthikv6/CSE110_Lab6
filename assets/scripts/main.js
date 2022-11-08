cc91ef9d96c80af3923b4820620b33a0d0db839e
karthikv6 <44593407+karthikv6@users.noreply.github.com>
2022-11-07 23:07:30 -0800

    main update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -160,9 +160,11 @@ function initFormHandler() {
   // Steps B12 & B13 will occur inside the event listener from step B11
   // B12. TODO - Clear the local storage
   // B13. TODO - Delete the contents of <main>
-  function submitFunc(){
-    tempFormData = Object.create(formElem);
-    
+  function clearLocal(){
+    window.localStorage.clear();
+
+    var mainElem = document.querySelector('main');
+    mainElem = '';
   }
 
 }
